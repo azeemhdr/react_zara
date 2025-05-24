97eaa7c4806cb18ec1456ebec19fe874b05ef5b3
azeemhdr <comazeem@gmail.com>
2025-05-25 03:01:58 +0530

    paragraph added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,11 +13,18 @@ export default function About() {
     h1:{
       color:"purple",
       fontSize:80
+    },
+    p:{
+      fontSize:20,
+      color:"#2c2c2c",
+      margin:20,
+      padding:25,
     }
   }
   return (
     <>
       <h1 style={{...styles.h1,...styles.services}}>Hello Zara</h1>
+      <p style={styles.services}>this is react style testing</p>
     </>
   );
 }
